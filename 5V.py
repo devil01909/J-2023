7a310a67431913f64abeb4e56bd2a3dc06cc449e
devil01909 <126428522+devil01909@users.noreply.github.com>
2023-04-15 15:19:47 +0600

    Update 5V.py

--- 5V.py
+++ 5V.py
@@ -32,7 +32,7 @@ def ud():
     print(' [2] EXIT')
     opt = input('\n   CHOOSE OPTION >>>\033[1;37m ')
     if opt == '1':
-        os.system('xdg-open https://www.youtube.com/@kawsarff609)
+        os.system('xdg-open https://www.youtube.com/@kawsarff609')
         FD()
         return None
     None('\n\x1b[1;31mEXIT\x1b[0;97m')
